60b94a194fe7c733c9d93b2c7e8aad8adc12e26a
Nanako7-ix <nanako7.ix@gmail.com>
2025-06-26 16:27:12 +0800

    增加关闭服务器

--- src/main/java/Server/RequestHandler.java
+++ src/main/java/Server/RequestHandler.java
@@ -275,7 +275,7 @@ public class RequestHandler implements Runnable {
      * 处理访问管理页面请求
      */
     private HttpResponse handleAdmin(HttpRequest request) {
-        System.out.println(123);
+//        System.out.println(123);
         String sessionId = getCookieValue(request, "sessionId");
         Session session = sessionId != null ? server.getSessions().get(sessionId) : null;
 
